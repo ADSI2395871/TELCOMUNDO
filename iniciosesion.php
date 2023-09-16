993671fe0d5e02c3eb8f946581bb29d1943a5894
ADSI2395871 <144939643+ADSI2395871@users.noreply.github.com>
2023-09-16 15:19:19 -0500

    Update iniciosesion.php

--- iniciosesion.php
+++ iniciosesion.php
@@ -6,12 +6,8 @@ try {
     if (isset($_POST["botton"])) {
 
         if ($_REQUEST['ID'] == null && $_REQUEST['contraseña'] == null) {
-            echo "
-            <script>
-                alert('Ingrese datos al formulario');
-                window.location = 'Index.php';    
-            </script>
-        ";
+            echo "<script>alert('Ingrese datos al formulario');window.location = 'Index.php';</script>";
+            exit; // Agregamos esto para detener la ejecución del script
         } else {
             $Usuario = $_POST['ID'];
             $Contraseña = $_POST['contraseña'];
@@ -34,15 +30,19 @@ try {
 
                 if ($filas['id_rol'] == 8) { // Lider    
                     header("location: Inicio1Lider.php");
+                    exit; // Agregamos esto para detener la ejecución del script
                 } elseif ($filas['id_rol'] == 9) { // Analista
                     header("location: Inicio2Analista.php");
+                    exit; // Agregamos esto para detener la ejecución del script
                 } elseif ($filas['id_rol'] == 10) { // Asesor
                     header("location: Inicio3Asesor.php");
+                    exit; // Agregamos esto para detener la ejecución del script
                 }
             } else {
                 // Aquí puedes manejar el caso en que $filas esté vacío o la consulta falle
                 // Redirige o muestra un mensaje de error apropiado
-                echo "<script>alert('USUARIO O CONTRASEÑA INCORRECTA'); window.location = 'Index.php';</script>";
+                echo "<script>alert('USUARIO O CONTRASEÑA INCORRECTA');window.location = 'Index.php';</script>";
+                exit; // Agregamos esto para detener la ejecución del script
             }
 
             sqlsrv_close($conecta);
