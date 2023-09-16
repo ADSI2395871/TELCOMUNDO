241b6709a8c5573493bbf27261763983031e53c0
ADSI2395871 <144939643+ADSI2395871@users.noreply.github.com>
2023-09-16 16:02:12 -0500

    Update iniciosesion.php

--- iniciosesion.php
+++ iniciosesion.php
@@ -1,8 +1,19 @@
-<?php
-ini_set('display_errors', 1);
-try {
-    include 'Controlador/Conexion.php';
+<?php 
+session_start();
+$connectionInfo = array("UID" => "CloudSAa141dee4", "pwd" => "Telcomundo*", "Database" => "Telcomundo", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:telcomundoserv.database.windows.net,1433";
+$conecta = sqlsrv_connect($serverName, $connectionInfo);
 
+if($conecta){
+    // CONEXION EXITOSA;
+}else{
+    // "fallo en la conexion";
+    die(print_r(sqlsrv_errors(), true));
+}
+
+ini_set('display_errors', 1);
+
+try {
     if (isset($_POST["botton"])) {
 
         if ($_REQUEST['ID'] == null && $_REQUEST['contraseña'] == null) {
