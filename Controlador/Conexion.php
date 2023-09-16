e1c72e43a0a34007d586397019140b674759db06
ADSI2395871 <144939643+ADSI2395871@users.noreply.github.com>
2023-09-16 15:32:40 -0500

    Update Conexion.php

--- Controlador/Conexion.php
+++ Controlador/Conexion.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 $connectionInfo = array("UID" => "CloudSAa141dee4", "pwd" => "Telcomundo*", "Database" => "Telcomundo", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:telcomundoserv.database.windows.net,1433";
 $conecta = sqlsrv_connect($serverName, $connectionInfo);
