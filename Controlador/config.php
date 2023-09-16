4b5917601f5e1e96680470996f38f65414f25eae
ADSI2395871 <144939643+ADSI2395871@users.noreply.github.com>
2023-09-16 17:27:01 -0500

    Update config.php

--- Controlador/config.php
+++ Controlador/config.php
@@ -2,9 +2,7 @@
 if (!defined('CONFIG_INCLUDED')) {
     define('CONFIG_INCLUDED', true);
 
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+    session_start();
    
     if (!isset($_SESSION['ID'])) {
         header("Location: index.php");
